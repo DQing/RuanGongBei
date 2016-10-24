b8b983bec8f3679faccddecc0f0f227e7d2c0fc1
douqing <15829258737@163.com>
2016-10-24 16:01:09 +0800

    完成商家和主页选需求页面

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function over() {
+    document.getElementById("dispare").innerHTML = "<div style='float: left;margin-top: -450px;font-size: 15px'>" +
+            "<h3></h3>"+
+        "<p><button>aaaa</button><span>aaaa</span><span>aaaa</span><span>aaaa</span></p>" +
+        "<p><span>aaaa</span><span>aaaa</span><span>aaaa</span><span>aaaa</span></p>" +
+        "<p><span>aaaa</span><span>aaaa</span><span>aaaa</span><span>aaaa</span></p>" +
+        "</div>";
+}
+function out() {
+    document.getElementById("dispare").innerHTML = document.getElementsByClassName("role");
+}
